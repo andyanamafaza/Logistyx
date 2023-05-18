4075942a5ec3c9d28fb8c0e80bf9cbbb25f46b6a
Andyana Lilmuttaqina Mafaza <andyanamafaza@gmail.com>
2023-05-18 16:47:21 +0700

    add new barchart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,7 +19,6 @@ class DashboardController extends Controller
         $supplier = Supplier::count();
         $gudang = Gudang::count();
 
-
         $tanggal_awal = date('Y-m-01');
         $tanggal_akhir = date('Y-m-d');
 
@@ -33,15 +32,31 @@ class DashboardController extends Controller
             $total_pembelian = Pembelian::where('created_at', 'LIKE', "%$tanggal_awal%")->sum('bayar');
 
             $pendapatan = $total_penjualan - $total_pembelian;
-            $data_pendapatan[] += $pendapatan;
+            $data_pendapatan[] = $pendapatan;
 
             $tanggal_awal = date('Y-m-d', strtotime("+1 day", strtotime($tanggal_awal)));
         }
 
         $tanggal_awal = date('Y-m-01');
 
+        $data_gudang = array();
+        $data_kepenuhan = array();
+
+        $gudangs = Gudang::all();
+
+        foreach ($gudangs as $gudang1) {
+            $data_gudang[] = $gudang1->nama_gudang;
+
+            $ukuran_gudang = $gudang1->ukuran_gudang;
+            $ukuran_awal = $gudang1->ukuran_awal;
+
+            $tingkat_kepenuhan = abs((($ukuran_gudang / $ukuran_awal) * 100)-100);
+            $tingkat_kepenuhan = round($tingkat_kepenuhan, 2);
+            $data_kepenuhan[] = $tingkat_kepenuhan;
+        }
+
         if (auth()->user()->level == 0) {
-            return view('admin.dashboard', compact('kategori', 'produk', 'supplier', 'gudang',  'tanggal_awal', 'tanggal_akhir', 'data_tanggal', 'data_pendapatan'));
+            return view('admin.dashboard', compact('kategori', 'produk', 'supplier', 'gudang', 'tanggal_awal', 'tanggal_akhir', 'data_tanggal', 'data_pendapatan', 'data_gudang', 'data_kepenuhan'));
         } else {
             return view('kasir.dashboard');
         }
